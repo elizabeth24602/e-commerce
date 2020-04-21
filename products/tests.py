1455b7d3206b679e3351bad1bd3703c73414ee0b
EC2 Default User <ec2-user@ip-172-31-86-208.ec2.internal>
2020-04-21 13:46:05 +0000

    updated products/tests.py

--- products/tests.py
+++ products/tests.py
@@ -8,6 +8,6 @@ class ProductTests(TestCase):
     Product model
     """
 
-     def test_str(self):
+def test_str(self):
         test_name = Product(name='A product')
         self.assertEqual(str(test_name), 'A product')
